f7b0af5ccd0812fc12d75cd70d565b336679aa6e
mystic3eyedraven <mystic3eyedraven@gmail.com>
2025-02-06 01:39:08 -0800

    updated code

--- backend.tf
+++ backend.tf
@@ -1,8 +1,17 @@
+##terraform {
+ # backend "s3" {
+ #   bucket         = "terraform-backend-primary-region"
+ #   key           = "docker-ecr/terraform.tfstate"
+#    region        = "us-west-2"
+ #   dynamodb_table = "terraform-backend-ddb"
+ # }
+##}
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-primary-region"
-    key           = "docker-ecr/terraform.tfstate"
-    region        = "us-west-2"
-    dynamodb_table = "terraform-backend-ddb"
+    bucket         = "mybucketfortoday"
+    key            = "docker-ecr/terraform-mystic3raven_docker-ecr-actions-workflow.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "TerraformLocks"
+    encrypt        = true
   }
 }
